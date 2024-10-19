87df299758faed9dbf44210b2a8081ae4810afdf
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-10-18 21:24:21 -0300

    Fullscreen added in .sobre div (PC only) !!!Need adjustments and bufixes

---
+++ scripts/fullScreen.js
@@ -0,0 +1,117 @@
+console.log("FullScreen.js loaded");
+document.addEventListener('DOMContentLoaded', () => {
+  const img = document.querySelector('#tns1-ow');
+  const svg = document.getElementById('fullscreen');
+  const overlay = document.getElementById('overlayFull');
+  
+  if (checkDim(1150)) {
+    
+    function fullscreen() {
+      if (img) {
+        enterFullscreen()
+        moveDivToEnd('.sobre-container', img.classList.toggle('fullscreen') ? '#body' : '.sobre');
+        img.classList.contains('fullscreen') ? (applyFullStyles(), overlay.style.display = 'block') : (revFullStyles(), exitFullscreen(), overlay.style.display = 'none');
+      } else {
+        console.error('Elemento img não encontrado');
+      }
+    }
+  
+    if (svg) {
+      svg.addEventListener('click', fullscreen);
+    } else {
+      console.error('Elemento svg não encontrado');
+    }
+  
+    function applyFullStyles() {
+      document.querySelectorAll('.item').forEach(item => {
+        item.style.width = '150vh'; item.style.height = '90vh'; item.style.position = 'fixed'; item.style.marginTop = '0'; item.style.boxShadow = 'none';
+      });
+      document.querySelectorAll('.tns-controls').forEach(control => {
+        control.style.top = '-22%'; control.style.gap = '105%';
+      });
+      document.querySelectorAll('.tns-controls button').forEach(button => {
+        button.style.transform = 'scale(1.7)';
+        button.addEventListener('mouseover', () => button.style.transform = 'scale(2)');
+        button.addEventListener('mouseout', () => button.style.transform = 'scale(1.7)');
+      });
+      const fullscreenElement = document.getElementById('fullscreen');
+      if (fullscreenElement) {
+        fullscreenElement.style.marginRight = '-5%'; fullscreenElement.style.marginTop = '27%';
+        changeSvgSize(fullscreenElement, 30, 30);
+      }
+    }
+  
+    function revFullStyles() {
+      document.querySelectorAll('.item').forEach(item => {
+        item.style.width = ''; item.style.height = ''; item.style.position = ''; item.style.marginTop = ''; item.style.boxShadow = '';
+      });
+      document.querySelectorAll('.tns-controls').forEach(control => {
+        control.style.top = ''; control.style.gap = '';
+      });
+      document.querySelectorAll('.tns-controls button').forEach(button => {
+        button.style.transform = 'scale(1)'; 
+        button.addEventListener('mouseover', () => button.style.transform = 'scale(1.5)'); 
+        button.addEventListener('mouseout', () => button.style.transform = 'scale(1)'); 
+      });
+      const fullscreenElement = document.getElementById('fullscreen');
+      if (fullscreenElement) {
+        fullscreenElement.style.marginRight = ''; fullscreenElement.style.marginTop = '';
+        changeSvgSize(fullscreenElement, 20, 20);
+      }
+    }
+
+  }
+  else{
+    return;
+  }
+  
+});
+
+function enterFullscreen() {
+  console.log('Fullscreen mode *ON*')
+  if (!document.fullscreenElement) {
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { // Firefox
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { // Chrome, Safari e Opera
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { // IE/Edge
+      element.msRequestFullscreen();
+    }
+  }
+}
+
+function exitFullscreen() {
+  console.log('Fullscreen mode *OFF*')
+  if (document.fullscreenElement)  {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari e Opera
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+      document.msExitFullscreen();
+    }
+  }
+}
+
+function checkDim(minWidth = 0, minHeight = 0) {
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < minWidth || window.innerHeight < minHeight) {
+      console.log('Dimensões fora dos limites.');
+      return false;
+    }
+    console.log('Dimensões dentro dos limites.');
+    return true;
+  });
+
+  if (window.innerWidth < minWidth || window.innerHeight < minHeight) {
+    console.log('Dimensões fora dos limites.');
+    return false;
+  }
+  console.log('Dimensões dentro dos limites.');
+  return true;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
   adjustControlsPosition();
   hideFirstButton();
   window.addEventListener('scroll', scrollHide);
-  // loadSections();
-  moveDivToEndAfterDelay();
+  moveDivToEndAfterDelay('#tns1-ow', '#sobre-container', 100);
+  moveDivToEndAfterDelay('#fullscreen', '.tns-controls', 100);
   addTitleToElement('.tns-controls button[data-controls="prev"]', 'Anterior')
   addTitleToElement('.tns-controls button[data-controls="next"]', 'Próximo')
+  addTitleToElement('#fullscreen', 'Tela Cheia')
   applyBorderToImagesInDivs('.item img');
   scrollAction(() => setOpacity(1, '.sobre h1', '.sobre p', '.sobre-container'), 500);
   scrollActionReverse(() => nav.style.backgroundColor = 'black', () => nav.style.backgroundColor = '', 550);
@@ -79,46 +80,6 @@ function applyBorderToImagesInDivs(div) {
   });
 }
 
-
-
-// Initialize slider
-function initializeSlider() {
-  console.log('criando slider');
-  slider = tns({
-    container: '.my-slider',
-    items: 1,
-    mode: 'gallery',
-    axis: 'vertical',
-    slideBy: "page",
-    arrowKeys: true,
-    viewportMax: true,
-    speed: 800,
-    autoplay: true,
-    autoplayTimeout: 4500,
-    controlsText: ["&#10094;", "&#10095"],
-    controlsPosition: 'bottom',
-    nav: false,
-    center: true,
-  });
-}
-
-// Adjust controls position
-function adjustControlsPosition() {
-  const controls = document.querySelector('.controls');
-  const innerChild = document.querySelector('.item');
-
-  if (controls && innerChild) {
-    const innerChildTop = parseInt(getComputedStyle(innerChild).top, 10);
-    controls.style.top = `${innerChildTop - 1000}px`;
-  }
-}
-
-// Hide the first button of the container
-function hideFirstButton() {
-  const firstButton = document.querySelector('#tns1-ow button:first-child');
-  if (firstButton) firstButton.style.display = 'none';
-}
-
 // Detect mobile devices
 const isMobile = () => /Mobi|Android/i.test(navigator.userAgent);
 
@@ -152,15 +113,10 @@ function loadFile(file_name, element_id) {
     .catch(error => console.error('Erro ao carregar arquivo:', error));
 }
 
-// Load sections
-// function loadSections() {
-//   ['navbar', 'footer', 'main'].forEach(section => loadFile(`sections/${section}.html`, section));
-// }
-
 // Move a div to the end of another div
 function moveDivToEnd(sourceDivId, targetDivId) {
-  const sourceDiv = document.getElementById(sourceDivId);
-  const targetDiv = document.getElementById(targetDivId);
+  const sourceDiv = document.querySelector(sourceDivId);
+  const targetDiv = document.querySelector(targetDivId);
 
   console.log("sourceDiv:", sourceDiv);
   console.log("targetDiv:", targetDiv);
@@ -174,9 +130,14 @@ function moveDivToEnd(sourceDivId, targetDivId) {
 }
 
 // Move div after delay
-function moveDivToEndAfterDelay() {
+function moveDivToEndAfterDelay(d1, d2, delay) {
   console.log("DOM totalmente carregado!");
   setTimeout(() => {
-    moveDivToEnd('tns1-ow', 'sobre-container');
-  }, 100);
+    moveDivToEnd(d1, d2);
+  }, delay);
+}
+
+function changeSvgSize(svgElement, newWidth, newHeight) {
+  svgElement.setAttribute('width', newWidth);
+  svgElement.setAttribute('height', newHeight);
 }
