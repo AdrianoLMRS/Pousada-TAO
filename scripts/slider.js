2159f48091e5fda818e18a8c95cf46352bd30aea
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-10-30 14:10:42 -0300

    Responsive added to testymony slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,52 +1,118 @@
 const currentTranslations = {};
 const items = document.querySelectorAll('.depo-slider .item-depo');
-const maxCount = items.length - 3;
+
 let iterationCount = 0; 
+let maxCount;
+let percentValue;
+ 
+function updateVariables() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth >= 850) { // Tela grande
+    percentValue = 108;
+    maxCount = items.length - 3;
+  } else if (screenWidth >= 650) { // Tela média
+    percentValue = 105.2
+    maxCount = items.length - 2;
+  } else { // Tela pequena
+    percentValue = 102.5
+    maxCount = items.length - 1;
+  }
+
+  iterationCount = Math.min(iterationCount, maxCount);
+}
+
+function updateButtonVisibility() {
+  const nextButton = document.getElementById('next-button');
+  const prevButton = document.getElementById('prev-button');
+
+  // Torna o botão 'next' invisível e não interativo quando estamos no começo (mínimo)
+  if (iterationCount === 0) {
+    nextButton.style.visibility = 'hidden';
+    nextButton.style.pointerEvents = 'none';
+  } else {
+    nextButton.style.visibility = 'visible';
+    nextButton.style.pointerEvents = 'auto';
+  }
+
+  // Torna o botão 'prev' invisível e não interativo quando estamos no final (máximo)
+  if (iterationCount === maxCount) {
+    prevButton.style.visibility = 'hidden';
+    prevButton.style.pointerEvents = 'none';
+  } else {
+    prevButton.style.visibility = 'visible';
+    prevButton.style.pointerEvents = 'auto';
+  }
+}
 
 function applyTranslateX(element, value) {
   element.style.transform = `translateX(${value})`;
 }
 
-function prevItem() {
-  // Incrementa o contador até o limite do maxCount
+function nextItem() {
   if (iterationCount < maxCount) {
     iterationCount++;
 
     items.forEach((item, index) => {
-      // Inicializa o deslocamento, se necessário
       if (!currentTranslations[index]) {
         currentTranslations[index] = 0;
       }
-      // Move cada item -108% adicional
-      currentTranslations[index] -= 108;
+      currentTranslations[index] -= percentValue;
       applyTranslateX(item, currentTranslations[index] + '%');
     });
-  } else {
-    console.log("Limite mínimo alcançado.");
-  }
-}
-
-function nextItem() {
-  // Decrementa o contador até 0
-  if (iterationCount > 0) {
-    iterationCount--;
-
-    items.forEach((item, index) => {
-      // Inicializa o deslocamento, se necessário
-      if (!currentTranslations[index]) {
-        currentTranslations[index] = 0;
-      }
-      // Move cada item +108% para retornar
-      currentTranslations[index] += 108;
-      applyTranslateX(item, currentTranslations[index] + '%');
-    });
+    updateButtonVisibility();
   } else {
     console.log("Limite máximo alcançado.");
   }
 }
 
+function prevItem() {
+  if (iterationCount > 0) {
+    iterationCount--;
+
+    items.forEach((item, index) => {
+      if (!currentTranslations[index]) {
+        currentTranslations[index] = 0;
+      }
+      currentTranslations[index] += percentValue;
+      applyTranslateX(item, currentTranslations[index] + '%');
+    });
+    updateButtonVisibility();
+  } else {
+    console.log("Limite mínimo alcançado.");
+  }
+}
+
+// function startAutoplay() {
+//   autoplayInterval = setInterval(() => {
+//     if (iterationCount === maxCount) {
+//       // Se chegar ao final, remove a transformação e redefine o contador
+//       items.forEach((item) => {
+//         item.style.transform = ''; // Remove a transformação
+//       });
+//       iterationCount = 0; // Reinicia o contador
+//       updateButtonVisibility();
+//       updateVariables();
+//     } else {
+//       nextItem(); // Avança para o próximo item
+//       updateButtonVisibility();
+//       updateVariables();
+//     }
+//   }, 4000); // Intervalo de 4000 ms
+// }
 
 
-
-
+updateButtonVisibility();
+updateVariables();
+startAutoplay();
+window.addEventListener('resize', () => {
+  // Chama applyTranslateX para cada item, passando '0%' para cada um
+  items.forEach((item) => {
+    item.style.transform = '';
+  });
   
+  updateVariables();
+  updateButtonVisibility();
+
+});
+
