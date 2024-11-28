5226d8fbc754909ccafb84ebeca41572ed39ef7e
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-11-28 19:03:06 -0300

    User can make more reservations

--- src/controllers/reservaController.js
+++ src/controllers/reservaController.js
@@ -1,13 +1,97 @@
-// *Dependecies
-    const path = require('path');
-    require('dotenv').config({ path: path.join(__dirname, '../.env') });
-    const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-    const {    calculateTotalPrice, validateDates,  } = require('../utils/reservaUtils')
-    const {  encrypt  } = require('../utils/bcryptUtils')
+// *Dependencies
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { calculateTotalPrice, validateDates } = require('../utils/reservaUtils');
+const jwt = require('jsonwebtoken');
+
+// Function to decode the JWT token from the cookie and return the decoded data
+function decodeAuthToken(req) {
+    const token = req.cookies.authToken; // Get the authToken from the cookie
+
+    if (!token) {
+        throw new Error('No token found');
+    }
+
+    try {
+        // Decode the JWT token using your JWT_SECRET
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        return decoded; // Return the decoded data
+    } catch (error) {
+        throw new Error('Error decoding token: ' + error.message);
+    }
+}
+
+// Function to create a session when customerId exists (using customerId)
+async function sessionWithId(customerId, totalPrice, checkin, checkout, adults, children) {
+    return await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'brl',
+                    product_data: {
+                        name: `Reserva para ${adults} adultos e ${children} crianças`,
+                        description: `Check-In: ${checkin.toLocaleDateString('pt-BR')}, Check-Out: ${checkout.toLocaleDateString('pt-BR')}`,
+                    },
+                    unit_amount: totalPrice,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${process.env.BASE_URL}/api/set-cookies?cs={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.BASE_URL}/reserva/canceled`,
+        metadata: {
+            checkIn: checkin.toLocaleDateString('pt-BR'),
+            checkOut: checkout.toLocaleDateString('pt-BR'),
+            adultos: adults,
+            criancas: children,
+        },
+        customer: customerId,
+        phone_number_collection: {
+            enabled: true, // Collect phone number
+        },
+    });
+}
+
+// Function to create a session without customerId (normal session)
+async function sessionNormal(totalPrice, checkin, checkout, adults, children, email, name, phone, address) {
+    return await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'brl',
+                    product_data: {
+                        name: `Reserva para ${adults} adultos e ${children} crianças`,
+                        description: `Check-In: ${checkin.toLocaleDateString('pt-BR')}, Check-Out: ${checkout.toLocaleDateString('pt-BR')}`,
+                    },
+                    unit_amount: totalPrice,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${process.env.BASE_URL}/api/set-cookies?cs={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.BASE_URL}/reserva/canceled`,
+        metadata: {
+            checkIn: checkin.toLocaleDateString('pt-BR'),
+            checkOut: checkout.toLocaleDateString('pt-BR'),
+            adultos: adults,
+            criancas: children,
+        },
+        customer_creation: 'always', // Always create a Stripe customer
+        customer_email: email, // Collect email
+        phone_number_collection: {
+            enabled: true, // Collect phone number
+        },
+    });
+}
 
 // Controller function to handle creating a checkout session
 const createCheckoutSession = async (req, res) => {
-    const { checkinDate, checkoutDate, adults, children } = req.body;
+    const { checkinDate, checkoutDate, adults, children, email, name, phone, address } = req.body;
 
     if (!checkinDate || !checkoutDate || !adults || !children) {
         return res.status(400).json({ error: 'Faltam dados obrigatórios.' });
@@ -18,36 +102,24 @@ const createCheckoutSession = async (req, res) => {
         const { checkin, checkout, days } = validateDates(checkinDate, checkoutDate);
         const totalPrice = calculateTotalPrice(days, adults, children);
 
-        // Create Stripe session directly in the controller
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'brl',
-                        product_data: {
-                            name: `Reserva para ${adults} adultos e ${children} crianças`,
-                            description: `Check-In: ${checkin.toLocaleDateString('pt-BR')}, Check-Out: ${checkout.toLocaleDateString('pt-BR')}`,
-                        },
-                        unit_amount: totalPrice,
-                    },
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
-            success_url: `${process.env.BASE_URL}/api/set-cookies?cs={CHECKOUT_SESSION_ID}`,
-            cancel_url: `${process.env.BASE_URL}/reserva/canceled`,
-            metadata: {
-                checkIn: checkin.toLocaleDateString('pt-BR'),
-                checkOut: checkout.toLocaleDateString('pt-BR'),
-                adultos: adults,
-                criancas: children,
-            },
-            customer_creation: 'always', // Always create a Stripe customer
-            phone_number_collection: {
-                enabled: true, // Collect phone number
-            },
-        });
+        // Decode JWT and extract customerId
+        let customerId = null;
+        try {
+            const decodedToken = decodeAuthToken(req);
+            customerId = decodedToken.customerId; // Extract customerId from decoded token
+        } catch (err) {
+            console.log('Token invalid or missing, proceeding without customerId.\n', err);
+        }
+
+        let session;
+
+        // If customerId exists, create session with customerId
+        if (customerId) {
+            session = await sessionWithId(customerId, totalPrice, checkin, checkout, adults, children);
+        } else {
+            // If no customerId, create session without it
+            session = await sessionNormal(totalPrice, checkin, checkout, adults, children, email, name, phone, address);
+        }
 
         res.status(200).json({ sessionId: session.id });
     } catch (error) {
