f34fc74b5c5d315d30ff24c8d8d5b45b4069d0d1
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-11-30 17:30:46 -0300

    New about section

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,98 +1,21 @@
-const currentTranslations = {};
-const items = document.querySelectorAll('.depo-slider .item-depo');
+// script.js
+const slider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-let iterationCount = 0; 
-let maxCount;
-let percentValue;
- 
-function updateVariables() {
-  const screenWidth = window.innerWidth;
+let currentIndex = 0;
 
-  if (screenWidth >= 850) { // Tela grande
-    percentValue = 108;
-    maxCount = items.length - 3;
-  } else if (screenWidth >= 650) { // Tela média
-    percentValue = 105.2
-    maxCount = items.length - 2;
-  } else { // Tela pequena
-    percentValue = 102.4
-    maxCount = items.length - 1;
-  }
-
-  iterationCount = Math.min(iterationCount, maxCount);
-}
-
-function updateButtonVisibility() {
-  const nextButton = document.getElementById('next-button');
-  const prevButton = document.getElementById('prev-button');
-
-  // Torna o botão 'next' invisível e não interativo quando estamos no começo (mínimo)
-  if (iterationCount === 0) {
-    nextButton.style.visibility = 'hidden';
-    nextButton.style.pointerEvents = 'none';
-  } else {
-    nextButton.style.visibility = 'visible';
-    nextButton.style.pointerEvents = 'auto';
-  }
-
-  // Torna o botão 'prev' invisível e não interativo quando estamos no final (máximo)
-  if (iterationCount === maxCount) {
-    prevButton.style.visibility = 'hidden';
-    prevButton.style.pointerEvents = 'none';
-  } else {
-    prevButton.style.visibility = 'visible';
-    prevButton.style.pointerEvents = 'auto';
-  }
-}
-
-function applyTranslateX(element, value) {
-  element.style.transform = `translateX(${value})`;
-}
-
-function nextItem() {
-  if (iterationCount < maxCount) {
-    iterationCount++;
-
-    items.forEach((item, index) => {
-      if (!currentTranslations[index]) {
-        currentTranslations[index] = 0;
-      }
-      currentTranslations[index] -= percentValue;
-      applyTranslateX(item, currentTranslations[index] + '%');
-    });
-    updateButtonVisibility();
-  } else {
-    console.log("Limite máximo alcançado.");
-  }
-}
-
-function prevItem() {
-  if (iterationCount > 0) {
-    iterationCount--;
-
-    items.forEach((item, index) => {
-      if (!currentTranslations[index]) {
-        currentTranslations[index] = 0;
-      }
-      currentTranslations[index] += percentValue;
-      applyTranslateX(item, currentTranslations[index] + '%');
-    });
-    updateButtonVisibility();
-  } else {
-    console.log("Limite mínimo alcançado.");
-  }
-}
-
-updateButtonVisibility();
-updateVariables();
-window.addEventListener('resize', () => {
-  // Chama applyTranslateX para cada item, passando '0%' para cada um
-  items.forEach((item) => {
-    item.style.transform = '';
-  });
-  
-  updateVariables();
-  updateButtonVisibility();
-
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex === 0) ? slider.children.length - 1 : currentIndex - 1;
+  updateSlider();
 });
 
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex === slider.children.length - 1) ? 0 : currentIndex + 1;
+  updateSlider();
+});
+
+function updateSlider() {
+  const width = slider.clientWidth;
+  slider.style.transform = `translateX(-${currentIndex * width}px)`;
+}
