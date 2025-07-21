33b778e2195d68871e2a9322f006d14fa9e0363f
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-07-21 14:53:40 -0300

    Fix: render.sh Gh API URL

--- website/render.sh
+++ website/render.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+REPO="Pousada-TAO"
+USER="AdrianoLMRS"
+
 set -e
 
 # Loads .env variables
@@ -19,7 +22,7 @@ fi
 echo "🔍 Searching last success run"
 
 RUN_ID=$(curl -s -H "Authorization: token $GITHUB_PAT" \
-    "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/workflows/$WORKFLOW_ID/runs?status=success&per_page=1" |
+    "https://api.github.com/repos/$USER/$REPO/actions/workflows/$WORKFLOW_ID/runs?status=success&per_page=1" |
     jq '.workflow_runs[0].id')
 
 
@@ -31,7 +34,7 @@ fi
 echo "🔎 Searching for artifact ID ($RUN_ID)..."
 
 ARTIFACT_ID=$(curl -s -H "Authorization: token $GITHUB_PAT" \
-    "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/runs/$RUN_ID/artifacts" | \
+    "https://api.github.com/repos/$USER/$REPO/actions/runs/$RUN_ID/artifacts" | \
     jq -r '.artifacts[] | select(.name=="site-dist") | .id')
 
 if [ -z "$ARTIFACT_ID" ]; then
@@ -45,7 +48,7 @@ echo "⬇️ Downloading & extracting dist..."
 rm -rf dist
 curl -L -H "Authorization: token $GITHUB_PAT" \
     -H "Accept: application/vnd.github+json" \
-    "https://api.github.com/repos/AdrianoLMRS/AndreValerio/actions/artifacts/$ARTIFACT_ID/zip" \
+    "https://api.github.com/repos/$USER/$REPO/actions/artifacts/$ARTIFACT_ID/zip" \
     -o site-dist.zip
 
 unzip site-dist.zip -d dist
