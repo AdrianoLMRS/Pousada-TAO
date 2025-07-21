936d2a02f03eab8a9f7dadceb98630f0f855142e
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-07-21 16:21:55 -0300

    Fix: Resolve URL GH-pages & Render

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite"; // Load environment variables
 
+const { PUBLIC_IS_GH_PAGES } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const isGhPages = PUBLIC_IS_GH_PAGES === 'true';
+
+// * Integrations
 import sitemap from '@astrojs/sitemap';
-
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pousada-tao.onrender.com',
-
+  site: isGhPages ? 'https://adrianolmrs.github.io' : 'https://pousada-tao.onrender.com',
+  base: isGhPages ? '/Pousada-TAO' : undefined,
   vite: {
       resolve: {
           alias: {
@@ -16,10 +20,8 @@ export default defineConfig({
           },
       },
   },
-
   image: {
       responsiveStyles: true,
   },
-
   integrations: [sitemap(), robotsTxt()],
 });
