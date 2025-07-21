07023bf38e9682c3fe0aaf758720b1aafe27bd85
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-07-21 15:49:51 -0300

    Feat: Add Astro Integrations
    Astro Sitemap & Astro robots.txt

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://pousada-tao.onrender.com',
+
+  vite: {
+      resolve: {
+          alias: {
+              '@': String(new URL('./src', import.meta.url)),
+          },
+      },
+  },
+
+  image: {
+      responsiveStyles: true,
+  },
+
+  integrations: [sitemap(), robotsTxt()],
+});
